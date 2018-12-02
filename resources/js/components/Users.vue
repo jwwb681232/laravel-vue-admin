bfbfa51154eb560ada3ef1f2377d1ace0766f001
wangxiao <wx497657341@qq.com>
2018-12-02 15:09:07 +0800

    users list

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -25,13 +25,15 @@
                                 <th>Name</th>
                                 <th>Email</th>
                                 <th>Type</th>
+                                <th>Created At</th>
                                 <th>Modify</th>
                             </tr>
-                            <tr>
-                                <td>183</td>
-                                <td>John Doe</td>
-                                <td>11-7-2014</td>
-                                <td><span class="tag tag-success">Approved</span></td>
+                            <tr v-for="user in users" :key="user.id">
+                                <td>{{user.id}}</td>
+                                <td>{{user.name}}</td>
+                                <td>{{user.email}}</td>
+                                <td>{{user.type}}</td>
+                                <td>{{user.created_at}}</td>
                                 <td>
                                     <a href=""><i class="fa fa-edit blue"></i></a>
                                     /
@@ -140,6 +142,7 @@
     export default {
         data() {
             return {
+                users:{},
                 form: new Form({
                     name: '',
                     email: '',
@@ -150,6 +153,11 @@
             }
         },
         methods:{
+            loadUsers(){
+                axios.get('api/user').then(({data})=>{
+                    this.users = data.data
+                });
+            },
             createUser(){
                 this.form.post('api/user').then((res)=>{
                     console.log(res)
@@ -157,7 +165,7 @@
             }
         },
         mounted() {
-            console.log('Component mounted.')
+            this.loadUsers();
         }
     }
 </script>
